a44abe5d11e5ce584f869adffb95a6b63c73ed9c
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-22 15:25:32 -0800

    set up index and fetch-utils to talk to supabase

---
+++ fetch-utils.js
@@ -0,0 +1,6 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqZGlkdWZ4emlzZnV2ZGZvYXdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTM0MzMsImV4cCI6MTk2MzEyOTQzM30.7x68WymKgxiG9t23XJ2axQIdjNViOFe6hF7MjD-qeQ8';
+
+const SUPABASE_URL = 'https://ajdidufxzisfuvdfoawq.supabase.co';
+
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
